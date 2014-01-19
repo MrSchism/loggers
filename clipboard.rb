d5d2d3d57b90b4b1006b3243d36edfd9b8ab0faa
Joe Preston <jpreston86@gmail.com>
2014-01-18 21:44:22 -0500

    Update clipboard.rb
    Added file existence check. If no file found, one is created with given name and made writable.

--- clipboard.rb
+++ clipboard.rb
@@ -1,6 +1,7 @@
 require 'clipboard'	# Ruby Gem to make the copy/paste of the clipboard easier
 require 'ffi-1.9.3'	# Ruby Gem to make cliipboard gem functional with Windows
 
+File.new(filename, 'w') unless File.exist?(filename)
 
 class Cliplog		# Clipboard Log class
     include Clipboard
